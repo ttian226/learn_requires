3157da41c1e22c027a366519856c79b75ed58d32
wangxu <ttian226@gmail.com>
2015-10-27 17:22:29 +0800

    u

--- my_require.js
+++ my_require.js
@@ -14,9 +14,24 @@ var requirejs, require, define;
         defContextName = '_',
         contexts = {};
 
+    function isFunction(it) {
+        return ostring.call(it) === '[object Function]';
+    }
 
     function isArray(it) {
         return ostring.call(it) === '[object Array]';
+    }
+
+    // 遍历数组
+    function each(ary, func) {
+        if (ary) {
+            var i;
+            for (i = 0; i < ary.length; i++) {
+                if (ary[i] && func(ary[i], i, ary)) {
+                    break;
+                }
+            }
+        }
     }
 
     function hasProp(obj, prop) {
@@ -25,6 +40,17 @@ var requirejs, require, define;
 
     function getOwn(obj, prop) {
         return hasProp(obj, prop) && obj[prop];
+    }
+
+    function eachProp(obj, func) {
+        var prop;
+        for (prop in obj) {
+            if (hasProp(obj, prop)) {
+                if (func(obj[prop], prop)) {
+                    break;
+                }
+            }
+        }
     }
 
     function eachReverse(ary, func) {
@@ -38,15 +64,92 @@ var requirejs, require, define;
         }
     }
 
+    function mixin(target, source, force, deepStringMixin) {
+        if (source) {
+            // 遍历source属性
+            eachProp(source, function (value, prop) {
+                // 如果force=true或target没有相同的属性prop时
+                if (force || !hasProp(target, prop)) {
+                    // 当value是一个对象时
+                    if (deepStringMixin && typeof value === 'object' && value &&
+                        !isArray(value) && !isFunction(value) && !(value instanceof RegExp)) {
+
+                        if (!target[prop]) {
+                            target[prop] = {};
+                        }
+                        mixin(target[prop], value, force, deepStringMixin);
+                    } else {
+                        target[prop] = value;
+                    }
+                }
+            });
+        }
+        return target;
+    }
+
     function scripts() {
         return document.getElementsByTagName('script');
     }
 
     function newContext(contextName) {
-        var context;
+        var context,
+            config = {
+                waitSeconds: 7,
+                baseUrl: './',
+                paths: {},
+                bundles: {},
+                pkgs: {},
+                shim: {},
+                config: {}
+            },
 
         context = {
             configure: function (cfg) {
+                // 确保baseUrl以`/`结尾
+                if (cfg.baseUrl) {
+                    if (cfg.baseUrl.charAt(cfg.baseUrl.length - 1) !== '/') {
+                        cfg.baseUrl += '/';
+                    }
+                }
+
+                var shim = config.shim,
+                    // objs里的属性值存放的都是对象
+                    objs = {
+                        paths: true,
+                        bundles: true,
+                        config: true,
+                        map: true
+                    };
+
+                // 把cfg合并到config
+                eachProp(cfg, function (value, prop) {
+                    // objs[prop]存在时，value是一个对象
+                    if (objs[prop]) {
+                        if (!config[prop]) {
+                            config[prop] = {};
+                        }
+                        // 把value对象合并到config[prop]中
+                        mixin(config[prop], value, true, true);
+                    } else {
+                        config[prop] = value;
+                    }
+                });
+
+                if (cfg.bundles) {
+
+                }
+
+                if (cfg.shim) {
+
+                }
+
+                if (cfg.packages) {
+
+                }
+
+                if (cfg.deps || cfg.callback) {
+                    context.require(cfg.deps || [], cfg.callback);
+                }
             },
             makeRequire: function () {
                 function localRequire(deps, callback, errback) {
@@ -70,6 +173,11 @@ var requirejs, require, define;
 
         if (!isArray(deps) && typeof deps !== 'string') {
             config = deps;
+            if (isArray(callback)) {
+
+            } else {
+                deps = [];
+            }
         }
 
         if (config && config.context) {
@@ -85,7 +193,7 @@ var requirejs, require, define;
             context.configure(config);
         }
 
-        return context.require(deps, callback, errback);
+        //return context.require(deps, callback, errback);
     };
 
     req.version = version;
@@ -96,6 +204,8 @@ var requirejs, require, define;
         contexts: contexts,
         newContext: newContext
     };
+
+    //req({});
 
     if (isBrowser) {
         head = s.head = document.getElementsByTagName('head')[0];
